3c3d34c4816e6bed60c4b536dbc89e69fb663062
Amnon Drory <amnon.drory@gmail.com>
2021-08-01 16:35:47 +0300

    Best-Buddies search is now done on GPU instead of CPU, making oveall runtime around 15% faster.

--- code/bb_pc/net/BBR_F.py
+++ code/bb_pc/net/BBR_F.py
@@ -1,60 +1,10 @@
 import numpy as np
 import torch
 from copy import deepcopy
-from sklearn.neighbors import NearestNeighbors
-import sortednp as snp
 
 from ..utils.tools_3d import euler_angles_to_rotation_matrix_torch, apply_rot_trans
 from ..utils.normals import point_to_plane_dist_sparse_torch
 from ..net.net_steps import record_in_logs
-
-def bidirectional_nearest_neighbors_postprocess(A, Brot, j_inds_h, i_inds_v, K=1):
-    i_inds_h = np.repeat(np.arange(A.shape[0]).reshape([-1, 1]), K, axis=1)
-    j_inds_v = np.repeat(np.arange(Brot.shape[0]).reshape([-1, 1]), K, axis=1)
-    return i_inds_h, j_inds_h, i_inds_v, j_inds_v
-
-
-def bidirectional_nearest_neighbors(A, Brot, K=1):
-    NN = NearestNeighbors(n_neighbors=1,
-                          radius=None,
-                          algorithm='kd_tree',
-                          leaf_size=30,
-                          metric='minkowski',
-                          p=2,
-                          metric_params=None,
-                          n_jobs=None)
-
-    NN.fit(Brot)
-    j_inds_h = NN.kneighbors(A, K, return_distance=False)
-
-    NN.fit(A)
-    i_inds_v = NN.kneighbors(Brot, K, return_distance=False)
-
-    return bidirectional_nearest_neighbors_postprocess(A, Brot, j_inds_h, i_inds_v, K)
-
-
-def prep_BBP_inds_for_soft_argmin(A, Brot):
-    # Quick CPU run to find the nearest neighbors in (rotated) B for each point in A,
-    # and vice versa.
-
-    i_inds_h, j_inds_h, i_inds_v, j_inds_v = bidirectional_nearest_neighbors(A, Brot)
-
-    num_points_B = Brot.shape[0]
-
-    comb_inds_h = num_points_B * i_inds_h.flatten() + j_inds_h.flatten()
-    comb_inds_v = num_points_B * i_inds_v.flatten() + j_inds_v.flatten()
-    comb_inds_h_s = np.sort(comb_inds_h)
-    comb_inds_v_s = np.sort(comb_inds_v)
-    comb_inds_pairs = snp.intersect(comb_inds_h_s, comb_inds_v_s)
-    i_inds_both, j_inds_both = np.divmod(comb_inds_pairs, num_points_B)
-    pairs = [i_inds_both.astype(int), j_inds_both.astype(int)]
-
-    inds = {}
-    inds['HARD_BEST_BUDDY_PAIRS'] = {}
-    inds['HARD_BEST_BUDDY_PAIRS']['A'] = pairs[0]
-    inds['HARD_BEST_BUDDY_PAIRS']['B'] = pairs[1]
-
-    return inds
 
 
 def prep_sparse_subset_of_points_torch(torch_A, torch_B, torch_A_normals, torch_B_normals, torch_inds):
@@ -68,18 +18,6 @@ def prep_sparse_subset_of_points_torch(torch_A, torch_B, torch_A_normals, torch_
     torch_points = {'points': torch_points, 'normals': torch_normals}
 
     return torch_points
-
-
-def convert_inds_to_torch(inds):
-    torch_inds = {}
-    for k1 in inds.keys():
-        torch_inds[k1] = {}
-        for k2 in inds[k1].keys():
-            torch_inds[k1][k2] = torch.tensor(inds[k1][k2], requires_grad=False)
-            if torch.cuda.is_available():
-                torch_inds[k1][k2] = torch_inds[k1][k2].cuda()
-
-    return torch_inds
 
 
 def SG_apply_rot_trans_torch(torch_points, WT, transT):
@@ -105,14 +43,84 @@ def calc_loss(torch_points, rotated_points):
 
     return BD
 
-def prerun_cpu(A, B, theta, phi, psi, trans_x, trans_y, trans_z):
-    angles = np.degrees([theta.item(), phi.item(), psi.item()])
-    trans = np.array([trans_x.item(), trans_y.item(), trans_z.item()])
-    Brot = apply_rot_trans(B, angles, trans)
-    Brot = Brot.astype(np.float32)
-    sparse_inds = prep_BBP_inds_for_soft_argmin(A, Brot)
-    torch_inds = convert_inds_to_torch(sparse_inds)
-    return torch_inds
+
+def torch_intersect(Na, Nb, i_ab,j_ab,i_ba,j_ba):    
+    def make_sparse_mat(i_,j_,sz):
+        inds = torch.cat([i_.reshape([1,-1]),
+                            j_.reshape([1,-1])],dim=0)
+        vals = torch.ones_like(inds[0,:])
+        
+        M = torch.sparse.FloatTensor(inds,vals,sz)
+        return M
+
+    sz = [Na,Nb]
+    M_ab = make_sparse_mat(i_ab,j_ab,sz)
+    M_ba = make_sparse_mat(i_ba,j_ba,sz)
+
+    M = M_ab.add(M_ba).coalesce()
+    i, j = M._indices()
+    v = M._values()
+    is_both = (v == 2)
+    i_final = i[is_both]
+    j_final = j[is_both]
+
+    return i_final, j_final
+
+
+def knn_gpu(P, Q):
+    nn_max_n = 500
+
+    def knn_dist(p, q):
+        # Fast implementation with torch.einsum()
+        with torch.no_grad():      
+            # L2 distance:
+            dist2 = torch.sum(p**2, dim=1).reshape([-1,1]) + torch.sum(q**2, dim=1).reshape([1,-1]) -2*torch.einsum('ac,bc->ab', p, q)
+            dist = dist2.clamp_min(1e-30).sqrt_()
+            # Cosine distance:
+            # dist = 1-torch.einsum('ac,bc->ab', p, q)                  
+            min_dist, ind = dist.min(dim=1, keepdim=True)      
+        return ind
+    
+    N = len(P)
+    C = int(np.ceil(N / nn_max_n))
+    stride = nn_max_n
+    inds = []
+    for i in range(C):
+        with torch.no_grad():
+            ind = knn_dist(P[i * stride:(i + 1) * stride], Q)
+            inds.append(ind)
+    
+    inds = torch.cat(inds)
+    assert len(inds) == N
+
+    corres_idx0 = torch.arange(len(inds), device=P.device).long().squeeze()
+    corres_idx1 = inds.long().squeeze()
+    return corres_idx0, corres_idx1
+
+
+def gpu_BB(P,Q):
+    corres_idx0, corres_idx1 = knn_gpu(P, Q)
+    
+    uniq_inds_1=torch.unique(corres_idx1)
+    inv_corres_idx1, inv_corres_idx0 = knn_gpu(Q[uniq_inds_1,:], P)
+    inv_corres_idx1 = uniq_inds_1
+
+    final_corres_idx0, final_corres_idx1 = torch_intersect(
+    P.shape[0], Q.shape[0],
+    corres_idx0, corres_idx1,
+    inv_corres_idx0, inv_corres_idx1)
+
+    return final_corres_idx0, final_corres_idx1
+
+
+def prerun_gpu(torch_A, torch_B, WT, transT):
+    torch_B_rot = (torch_B.double() @ WT) + transT
+    pairs0, pairs1 = gpu_BB(torch_A,torch_B_rot)
+    inds = {}
+    inds['HARD_BEST_BUDDY_PAIRS'] = {}
+    inds['HARD_BEST_BUDDY_PAIRS']['A'] = pairs0
+    inds['HARD_BEST_BUDDY_PAIRS']['B'] = pairs1
+    return inds
 
 
 def BBR_F_step(A, B, torch_A, torch_B,
@@ -131,7 +139,7 @@ def BBR_F_step(A, B, torch_A, torch_B,
     WT = W.T.double()
     transT = trans.T.double()
 
-    torch_inds = prerun_cpu(A, B, theta, phi, psi, trans_x, trans_y, trans_z)
+    torch_inds = prerun_gpu(torch_A, torch_B, WT, transT)
 
     torch_points = prep_sparse_subset_of_points_torch(torch_A, torch_B, torch_A_normals, torch_B_normals, torch_inds)
 
